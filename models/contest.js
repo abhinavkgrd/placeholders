a045eface732dcd6922cd007af56304e4e12417d
abhinav-grd <abhinavk.grd@gmail.com>
2020-07-29 10:11:15 +0530

    model mistakes fixed

--- models/contest.js
+++ models/contest.js
@@ -3,10 +3,11 @@ var Schema = mongoose.Schema;
 
 
 var contestSchema = new Schema({
-    name: {type:String},
+    name: { type: String },
     problems: [{ type: Schema.Types.ObjectId, ref: "Problem" }],
     submissions: [{ type: Schema.Types.ObjectId, ref: "Submission" }],
-    users: [{ details: { type: Schema.Types.ObjectId, ref: "User" }, AC_problems: { type: Schema.Types.ObjectId, ref: "Problem" }}],
+    users: [{ type: Schema.Types.ObjectId, ref: "User"}],
+    //change AC problems to score later
     start_time: Date,
     end_time: Date
 });
@@ -16,23 +17,21 @@ contestSchema
     .get(function () {
         return '/contests/' + this._id;
     });
-
 contestSchema
-    .virtual('status')
+    .virtual('code')
     .get(function () {
-        if (Date.now() < start_time)
-            return "upcoming";
-        else if (Date.now() >= start_time && Date.now() < end_time)
-            return "active";
-        else
-            return "past";
+        return String(this._id).slice(-5);
     });
 
-
 contestSchema
     .virtual('status')
     .get(function () {
-        return end_time - start_time;
+        if (Date.now() < this.start_time)
+            return "Upcoming";
+        else if (Date.now() >= this.start_time && Date.now() < this.end_time)
+            return "active";
+        else
+            return "Ended";
     });
 
 module.exports = mongoose.model('Contest', contestSchema);
--- models/user.js
+++ models/user.js
@@ -4,16 +4,17 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = new Schema({
   firstname: { type: String, required: true },
-  lastname: { type: String },
+  lastname: { type: String ,default:''},
   email: {type: String, required:true, unique:true}, 
   username: { type: String , required:true,unique:true},
   password: { type: String , required:true},
+  rating :{type:Number, default: 1200},
   submissions: [{ type: Schema.Types.ObjectId, ref: "Submission" }]
 });
 
 userSchema.virtual('display_name')
   .get(function () {
-    return String(first_name + ' ' + last_name);
+    return firstname + ' ' + lastname;
   });
 
 userSchema.virtual('url')
