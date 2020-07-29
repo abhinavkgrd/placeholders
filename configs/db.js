28fa5a1bd7ef859ec51b3ecc1c9c1d07b77de73d
abhinav-grd <abhinavk.grd@gmail.com>
2020-07-29 10:04:17 +0530

    using local db and debugging setup

--- configs/db.js
+++ configs/db.js
@@ -2,9 +2,10 @@
 var mongoose = require('mongoose');
 
 var mongoDB = 'mongodb+srv://dbuser:strongPass@cluster0-jn6qi.mongodb.net/Codeforces_clone_db?retryWrites=true&w=majority';
+var DBurl_local ='mongodb://localhost:27017/placeholders';
 const InitiateMongoServer = async () => {
     try {
-        await mongoose.connect(mongoDB, {
+        await mongoose.connect(DBurl_local, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
